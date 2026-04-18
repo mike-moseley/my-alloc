33f320ee5c309b38431f778cb1222b70da082163
Mike Moseley <mike.moseley@tuta.io>
2026-04-18 17:13:42 -0400

    Add tests for myFree function

--- test/alloc/test_myFree.c
+++ test/alloc/test_myFree.c
@@ -1,6 +1,7 @@
 #include "unity.h"
 #include "alloc.h"
 #include "unity_internals.h"
+#include <stdlib.h>
 void setUp(void) {}
 void tearDown(void) {}
 
@@ -13,8 +14,35 @@ void test_myFree(void) {
 	TEST_ASSERT_EQUAL(ptr, ptr2);
 	myFree(ptr2);
 }
+
 void test_myFree_null(void) {
 	myFree(NULL);
+}
+
+void test_consistent_heap(void) {
+	void *ptr;
+	void * ptr2;
+	void * ptr3;
+
+	ptr = myMalloc(16);
+	ptr2 = myMalloc(16);
+	ptr3 = myMalloc(16);
+
+	myFree(ptr2);
+	myFree(ptr);
+	myFree(ptr3);
+
+	ptr = myMalloc(16);
+	ptr2 = myMalloc(16);
+	ptr3 = myMalloc(16);
+
+	TEST_ASSERT_NOT_NULL(ptr);
+	TEST_ASSERT_NOT_NULL(ptr2);
+	TEST_ASSERT_NOT_NULL(ptr3);
+
+	myFree(ptr);
+	myFree(ptr2);
+	myFree(ptr3);
 }
 
 int main(void)
@@ -22,5 +50,6 @@ int main(void)
 	UNITY_BEGIN();
 	RUN_TEST(test_myFree);
 	RUN_TEST(test_myFree_null);
+	RUN_TEST(test_consistent_heap);
 	return UNITY_END();
 }
