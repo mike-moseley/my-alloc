f28e757a3818c58a234b3ffc792578503e7e0347
Mike Moseley <mike.moseley@tuta.io>
2026-04-18 19:16:39 -0400

    Add arenaCreate function in src/arena.c

---
+++ src/arena.c
@@ -0,0 +1,21 @@
+#include "alloc_error.h"
+#include "sys/mman.h"
+#include "arena.h"
+
+AllocError arenaCreate(size_t capacity, arena_t **out) {
+	arena_t *arena;
+
+	if(out == NULL) return ALLOC_ERROR_NULL;
+	if(capacity == 0) return ALLOC_ERROR_SIZE;
+
+	arena = (arena_t *)mmap(NULL, sizeof(arena_t) + capacity, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if(arena == MAP_FAILED) return ALLOC_ERROR_OOM;
+
+	/* buf is the data in the arena, must offset by the size of the header*/
+	arena->buf = (char *)arena + sizeof(arena_t);
+	arena->offset = 0;
+	arena->capacity = capacity;
+	*out = arena;
+
+	return ALLOC_OK;
+}
