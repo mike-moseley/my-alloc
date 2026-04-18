b94c1c92f2bca019b2869f736de008f4f23a7ee3
Mike Moseley <mike.moseley@tuta.io>
2026-04-18 17:37:16 -0400

    Add tests for myRealloc

--- test/alloc/test_myRealloc.c
+++ test/alloc/test_myRealloc.c
@@ -12,9 +12,39 @@ void test_myRealloc_returns_non_null(void) {
 	myFree(ptr);
 }
 
+void test_returns_null_0_size(void) {
+	void *ptr;
+	ptr = myMalloc(16);
+	ptr = myRealloc(ptr, 0);
+
+	TEST_ASSERT_NULL(ptr);
+}
+
+void test_shrink_same_ptr(void) {
+	void *ptr;
+	char *ptr_addr_pre_shrink;
+	ptr = myMalloc(16);
+	ptr_addr_pre_shrink = ptr;
+	ptr = myRealloc(ptr, 8);
+	TEST_ASSERT_EQUAL_PTR(ptr_addr_pre_shrink, ptr);
+}
+
+void test_grow_preserves_data(void) {
+	void *ptr;
+	long *ptr_data_pre_grow;
+	ptr = myMalloc(8);
+	*(long *)ptr = 1337;
+	ptr_data_pre_grow = (long *)ptr;
+	ptr = myRealloc(ptr, 16);
+	TEST_ASSERT_EQUAL(*ptr_data_pre_grow, *(long *)ptr);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_myRealloc_returns_non_null);
+	RUN_TEST(test_returns_null_0_size);
+	RUN_TEST(test_shrink_same_ptr);
+	RUN_TEST(test_grow_preserves_data);
 	return UNITY_END();
 }
