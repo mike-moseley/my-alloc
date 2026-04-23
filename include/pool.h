576b2cac576301ef2aae1ccdefd5ec7edcf762da
Mike Moseley <mike.moseley@tuta.io>
2026-04-23 07:11:42 -0400

    Finished poolCreate free_list implementation

--- include/pool.h
+++ include/pool.h
@@ -6,12 +6,12 @@
 
 typedef struct {
 	char *buf;
-	size_t capacity;
+	size_t count;
 	size_t chunk_size;
 	void *free_list;
 } pool_t;
 
-AllocError poolCreate(size_t capacity, size_t chunk_size, pool_t **out);
+AllocError poolCreate(size_t count, size_t chunk_size, pool_t **out);
 AllocError poolAlloc(pool_t *pool, void **out);
 void poolFree(pool_t *pool, void *chunk);
 void poolDestroy(pool_t *pool);
--- src/pool.c
+++ src/pool.c
@@ -1,11 +1,55 @@
 #include "pool.h"
-
-typedef struct pool_node_t pool_node_t;
-struct pool_node_t {
-	pool_node_t *next;
-};
+#include "alloc_error.h"
+#include "alloc_common.h"
+#include <sys/mman.h>
 
 typedef struct chunk_t chunk_t;
 struct chunk_t {
 	chunk_t *next;
 };
+
+static AllocError link_chunks(pool_t *pool, chunk_t *free_list) {
+	size_t i;
+	chunk_t *tmp_chunk;
+	chunk_t *chunk = free_list;
+
+	if(pool == NULL) return ALLOC_ERROR_NULL; 
+
+	/* Last iteration chunk->next must be null; handle after loop. */
+	for(i=0; i<pool->count-1; i++) {
+		tmp_chunk = (chunk_t *)((char *)chunk + pool->chunk_size);
+		chunk->next = tmp_chunk;
+		chunk = tmp_chunk;
+	}
+	chunk->next = NULL;
+
+	return ALLOC_OK;
+}
+
+AllocError poolCreate(size_t count, size_t chunk_size, pool_t **out) {
+	pool_t *pool;
+	size_t pool_size;
+	AllocError err;
+
+	if(count == 0) return ALLOC_ERROR_SIZE;
+	if(chunk_size == 0) return ALLOC_ERROR_SIZE;
+	if(out == NULL) return ALLOC_ERROR_NULL;
+
+	chunk_size = ALIGN(chunk_size);
+	pool_size = chunk_size * count;
+
+	pool = (pool_t *)mmap(NULL, sizeof(pool_t) + pool_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if(pool == MAP_FAILED) return ALLOC_ERROR_OOM;
+	
+	pool->buf = (char *)pool + sizeof(pool_t);
+	pool->chunk_size = chunk_size;
+	pool->count = count;
+	pool->free_list = (chunk_t *)pool->buf;
+	err = link_chunks(pool, pool->free_list);
+	if (err != ALLOC_OK) {
+		return err;
+	}
+	*out = pool;
+	
+	return ALLOC_OK;
+}
